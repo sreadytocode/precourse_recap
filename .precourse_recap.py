4fa5a946e11330441ad8e7611b4f51b241895ba5
sreadytocode <sara.younis2303@gmail.com>
2022-09-12 20:18:26 +0100

    first commit

---
+++ .precourse_recap.py
@@ -0,0 +1,10 @@
+#create variables of different types
+breakfast = "Overnight Oats"
+lunch = "Avocado toast with smoked Mackeral"
+dinner = "Pasta"
+
+
+def daily_main_meals():
+    print("Today I ate " + breakfast + " for breakfast, " + lunch + " for lunch and " + dinner + " for dinner.")
+
+daily_main_meals()
